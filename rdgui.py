6be68702f40542d9ab5464b831991d9023f66117
Jeremy Drake <github@jdrake.com>
2021-02-19 20:39:48 -0800

    Move data "reading" to background thread.
    Once it is actually querying the RD unit, it would be good to keep the
    IO off the UI thread.

--- rdgui.py
+++ rdgui.py
@@ -7,6 +7,7 @@ from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 import matplotlib.animation as animation
 import numpy as np
+import threading
 import wx
 
 import rdgui_xrc
@@ -16,15 +17,47 @@ submodpaths.add_to_path()
 from numpy_ringbuffer import RingBuffer
 
 
+def emitter(p=0.1):
+    """Return a random value in [0, 1) with probability p, else 0."""
+    while True:
+        v = np.random.rand(1)
+        if v > p:
+            yield 0.
+        else:
+            yield np.random.rand(1)
+
+class ReaderThread(threading.Thread):
+    def __init__(self):
+        super(ReaderThread, self).__init__(daemon=True)
+        self.running = True
+        self.datalock = threading.Lock()
+        self.shutdowncond = threading.Condition(self.datalock)
+        self.t = RingBuffer(200, float)
+        self.d = RingBuffer(200, float)
+
+    def cancel(self):
+        with self.datalock:
+            self.running = False
+            self.shutdowncond.notify()
+
+    def run(self):
+        gen = emitter()
+        with self.datalock:
+            while self.running:
+                t = time()
+                self.t.append(t)
+                self.d.append(next(gen))
+                self.shutdowncond.wait(max((t + 0.05) - time(), 0))
+
 class CanvasFrame(rdgui_xrc.xrcCanvasFrame):
     def __init__(self, parent=None):
         super(CanvasFrame, self).__init__(parent)
 
+        self.reader = ReaderThread()
+
         self.figure = Figure()
         axes = self.figure.add_subplot(111)
-        self.t = RingBuffer(175, float)
-        self.d = RingBuffer(175, float)
-        self.line = Line2D(self.t, self.d)
+        self.line = Line2D(self.reader.t, self.reader.d)
         axes.add_line(self.line)
         axes.set_ylim(-.1, 1.1)
         axes.set_xlim(-10, 0)
@@ -39,6 +72,7 @@ class CanvasFrame(rdgui_xrc.xrcCanvasFrame):
             'motion_notify_event', self.UpdateStatusBar)
 
         self.Fit()
+        self.reader.start()
 
     def UpdateStatusBar(self, event):
         if event.inaxes:
@@ -48,25 +82,17 @@ class CanvasFrame(rdgui_xrc.xrcCanvasFrame):
             self.SetStatusText("")
 
     def update(self, d):
-        t = time()
-        self.t.append(t)
-        self.d.append(d)
-        self.line.set_data(self.t - t, self.d)
+        with self.reader.datalock:
+            self.line.set_data(self.reader.t - time(), np.asarray(self.reader.d))
         return self.line,
 
     def OnMenu_wxID_EXIT(self, evt):
         self.Close()
 
-
-def emitter(p=0.1):
-    """Return a random value in [0, 1) with probability p, else 0."""
-    while True:
-        v = np.random.rand(1)
-        if v > p:
-            yield 0.
-        else:
-            yield np.random.rand(1)
-
+    def OnClose(self, evt):
+        self.reader.cancel()
+        self.reader.join()
+        evt.Skip()
 
 class App(wx.App):
     def OnInit(self):
@@ -74,7 +100,7 @@ class App(wx.App):
         frame = CanvasFrame()
         self.SetTopWindow(frame)
         # pass a generator in "emitter" to produce data for the update func
-        self.ani = animation.FuncAnimation(frame.figure, frame.update, emitter,
+        self.ani = animation.FuncAnimation(frame.figure, frame.update,
                 interval=50, blit=True)
 
         frame.Show(True)
