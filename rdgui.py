1cce1c8254c687175a3d781673d111c9753e70b3
Jeremy Drake <github@jdrake.com>
2021-02-21 22:16:56 -0800

    Add simple control of RD unit.
    SpinCtrlDoubles for set voltage and amperage, and a ToggleButton for
    enabling the output.
    
    Add a second field to the status bar, for the last read voltage and
    amperage.
    
    NOTE: there is unexpected behavior (bug?) with the SpinCtrlDouble, where
    writing a number in the text box and hitting "enter" triggers the
    default ("Update") button, but the edit in the text box is not applied
    to the control's value.

--- rdgui.py
+++ rdgui.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 from time import time
+import threading
+import traceback
 
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 import matplotlib.animation as animation
+import minimalmodbus
 import numpy as np
-import threading
 import wx
 
 import rdgui_xrc
@@ -32,15 +34,53 @@ def emitter(p=0.1):
         else:
             yield np.random.rand(1)
 
+class RD6006(rd6006.RD6006):
+    def __init__(self, *args, **kwargs):
+        super(RD6006, self).__init__(*args, **kwargs)
+
+    def _write_registers(self, register, values):
+        try:
+            return self.instrument.write_registers(register, values)
+        except minimalmodbus.NoResponseError:
+            return self._write_registers(register, values)
+
+    @property
+    def measvoltagecurrent(self):
+        reg = self._read_registers(10, 2)
+        return (reg[0] / self.voltres, reg[1] / self.ampres)
+
+    @property
+    def voltagecurrent(self):
+        reg = self._read_registers(8, 2)
+        return (reg[0] / self.voltres, reg[1] / self.ampres)
+
+    @voltagecurrent.setter
+    def voltagecurrent(self, value):
+        self._write_registers(8, [int(value[0] * self.voltres), int(value[1] * self.ampres)])
+
+class RDWrapper(object):
+    def __init__(self):
+        super(RDWrapper, self).__init__()
+        self.rd = None # type: RD6006
+        self.lock = threading.Lock()
+
+    def open(self, port):
+        with self.lock:
+            if self.rd is not None:
+                self.rd.instrument.serial.close()
+            self.rd = RD6006(port)
+
+
+rdwrap = RDWrapper()
+
 class ReaderThread(threading.Thread):
-    def __init__(self, port):
+    def __init__(self):
         # type: (str) -> None
         super(ReaderThread, self).__init__(daemon=True)
         config = wx.Config.Get() # type: wx.ConfigBase
         self.polling_interval = config.ReadFloat("polling_interval", POLLING_INTERVAL) # type: float
         self.mock = config.ReadBool("mock_data", MOCK_DATA) # type: bool
         graph_seconds = config.ReadFloat("graph_seconds", GRAPH_SECONDS) # type: float
-        self.port = port
         self.running = True
         self.datalock = threading.Lock()
         self.shutdowncond = threading.Condition(self.datalock)
@@ -57,17 +97,14 @@ class ReaderThread(threading.Thread):
         if self.mock:
             vgen = emitter()
             agen = emitter()
-        else:
-            rd = rd6006.RD6006(self.port)
         while self.running:
             t = time()
             if self.mock:
                 v = next(vgen)
                 a = next(agen)
             else:
-                reg = rd._read_registers(10, 2)
-                v = reg[0] / rd.voltres
-                a = reg[1] / rd.ampres
+                with rdwrap.lock:
+                    v, a = rdwrap.rd.measvoltagecurrent
             print (time() - t, v, a)
             with self.datalock:
                 self.t.append(t)
@@ -118,16 +155,34 @@ class CanvasFrame(rdgui_xrc.xrcCanvasFrame):
     def __init__(self, parent=None):
         super(CanvasFrame, self).__init__(parent)
 
+        # no-op self-assignment for typing
+        self.ctlVoltage = self.ctlVoltage   # type: wx.SpinCtrlDouble
+        self.ctlAmperage = self.ctlAmperage # type: wx.SpinCtrlDouble
+        self.btnEnable = self.btnEnable     # type: wx.ToggleButton
+
         config = wx.Config.Get() # type: wx.ConfigBase
         port = config.Read("port") # type: str
         if port == "":
-            dlg = DlgPortSelector(self)
-            if dlg.ShowModal() == wx.ID_OK:
-                port = dlg.port
-                config.Write("port", port)
+            with DlgPortSelector(self) as dlg:
+                if dlg.ShowModal() == wx.ID_OK:
+                    port = dlg.port
+                    config.Write("port", port)
 
-        self.reader = ReaderThread(port)
+        try:
+            rdwrap.open(port)
+            voltage, current = rdwrap.rd.voltagecurrent
+            # assume model number is (max_voltage * 100) + max_amperage
+            self.ctlVoltage.SetIncrement(1.0/rdwrap.rd.voltres)
+            self.ctlAmperage.SetMax(int(rdwrap.rd.type / 100) + 0.6)
+            self.ctlVoltage.SetValue(voltage)
+            self.ctlAmperage.SetIncrement(1.0/rdwrap.rd.ampres)
+            self.ctlAmperage.SetMax((rdwrap.rd.type % 100) + 0.6)
+            self.ctlAmperage.SetValue(current)
+            self.btnEnable.SetValue(rdwrap.rd.enable)
+        except:
+            traceback.print_exc()
 
+        self.reader = ReaderThread()
         self.figure = Figure()
 
         self.vaxis = self.figure.add_subplot(111)
@@ -167,26 +222,36 @@ class CanvasFrame(rdgui_xrc.xrcCanvasFrame):
                 v = self.vaxis.transData.inverted().transform((event.x, event.y))[1]
                 a = event.ydata
             self.SetStatusText(
-                "t={}  V={}  A={}".format(event.xdata, v, a))
+                "t={:.3f}  V={:.3f}  A={:.3f}".format(event.xdata, v, a))
         else:
             self.SetStatusText("")
 
     def update(self, d):
         with self.reader.datalock:
             t = time()
-            self.vline.set_data(self.reader.t - t, np.asarray(self.reader.v))
-            self.aline.set_data(self.reader.t - t, np.asarray(self.reader.a))
+            v = np.asarray(self.reader.v)
+            a = np.asarray(self.reader.a)
+            self.vline.set_data(self.reader.t - t, v)
+            self.aline.set_data(self.reader.t - t, a)
+            if self and len(v) > 0 and len(a) > 0:
+                self.SetStatusText("Last V={:.3f}  A={:.3f}".format(v[-1], a[-1]), 1)
         return self.vline, self.aline
 
+    def OnButton_btnUpdate(self, evt):
+        with rdwrap.lock:
+            rdwrap.rd.voltagecurrent = (self.ctlVoltage.GetValue(), self.ctlAmperage.GetValue())
+
+    def OnTogglebutton_btnEnable(self, evt):
+        # type: (wx.CommandEvent) -> None
+        with rdwrap.lock:
+            rdwrap.rd.enable = evt.IsChecked()
+
     def OnMenu_wxID_OPEN(self, evt):
-        port = self.reader.port
-        self.reader.cancel()
-        dlg = DlgPortSelector(self)
-        if dlg.ShowModal() == wx.ID_OK:
-            port = dlg.port
-            wx.Config.Get().Write("port", port)
-        self.reader = ReaderThread(port)
-        self.reader.start()
+        with DlgPortSelector(self) as dlg:
+            if dlg.ShowModal() == wx.ID_OK:
+                port = dlg.port
+                rdwrap.open(port)
+                wx.Config.Get().Write("port", port)
 
     def OnMenu_wxID_EXIT(self, evt):
         self.Close()
